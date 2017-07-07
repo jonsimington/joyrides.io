fb8785a4bcbe96213be8173c02ad0995fd589349
Jon Simington <jon.simington@mst.edu>
2017-07-07 03:57:49 -0500

    Initial Commit

---
+++ joyrides.io/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  joyrides.io
+//
+//  Created by Jon on 7/7/17.
+//  Copyright © 2017 jonsimington. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
